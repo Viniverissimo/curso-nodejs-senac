9ccbdcae13359eae94034465b5920eec7b618d64
VINICIUS VERISSIMO VIANA BEZERRA <vinicius.vvbezerra@senacsp.edu.br>
2025-11-08 11:49:14 -0300

    Aprendendo sobre rotas get, post, put e delete

--- API/index.js
+++ API/index.js
@@ -2,8 +2,33 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+let frutas = [
+  "Banana",
+  "Maçã",
+  "Laranja",
+  "Manga"
+];
+
+app.get ("/fruta", (req, res) => { 
+  res.send(frutas);
+});
+
+app.post("/fruta", (req, res) => { 
+  const novaFruta = req.body.nome;
+  frutas.push(novaFruta);
+  res.send(`Fruta ${novaFruta} adicionada com sucesso!`);
+}); 
+
+app.put("/fruta/:posicao",(req,res) =>{ 
+  frutas[req.params.posicao] = req.body.nome;
+  res.send(`Atualização realizada com sucesso!`);
+});
+
+app.delete("/fruta/:posicao", (req,res) =>{ 
+  const frutaRemovida = frutas.splice(req.params.posicao, 1);
+  res.send(`Fruta ${frutaRemovida} removida com sucesso!`);
 });
 
 const PORT = process.env.PORT || 3000;
